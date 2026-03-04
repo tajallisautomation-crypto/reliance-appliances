e6f47e38bc287886ac7f4325bb4963781e1c0c09
tajallisautomation-crypto <tajallisautomation@gmail.com>
2026-03-04 16:12:07 +0500

    v3.0: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          store:  ['zustand'],
+        }
+      }
+    }
+  }
+})
