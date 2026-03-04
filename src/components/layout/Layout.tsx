660cb7ce6cb9d22c017b86033d1ad39cfa4d35c1
tajallisautomation-crypto <tajallisautomation@gmail.com>
2026-03-04 16:12:58 +0500

    v3.0: src/components/layout/Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-white">
